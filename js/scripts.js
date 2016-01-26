a74faa3cacc85501d52a8a81ce1d6c23ee5ee7ac
= <=>
2016-01-25 16:58:42 -0800

    add fields to input and display multiple addresses for contact objects

--- js/scripts.js
+++ js/scripts.js
@@ -19,15 +19,39 @@ Address.prototype.fullAddress = function(street, city, state) {
 }
 
 $(document).ready(function() {
+  $("#add-address").click(function() {
+    $("#new-addresses").append('<div class="new-address">' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-street">Street</label>' +
+                                   '<input type="text" class="form-control new-street">' +
+                                 '</div>' +
+
+                                 '<div class="form-group">' +
+                                   '<label for="new-city">City</label>' +
+                                   '<input type="text" class="form-control new-street">' +
+                                  '</div>' +
+
+                                 '<div class="form-group">' +
+                                   '<label for="new-state">State</label>' +
+                                   '<input type="text" class="form-control new-street">' +
+                                  '</div>');
+  });
+
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
 
     var inputFirstName = $("input#new-first-name").val();
     var inputLastName = $("input#new-last-name").val();
-    var addresses = $("input")
     var newContact = new Contact(inputFirstName, inputLastName);
 
-    console.log(newContact);
+    $(".new-address").each(function() {
+      var inputStreet = $(this).find("input#new-street").val();
+      var inputCity   = $(this).find("input#new-city").val();
+      var inputState  = $(this).find("input#new-state").val();
+      var newAddress = new Address(inputStreet, inputCity, inputState);
+      newContact.addresses.push(newAddress);
+    });
+
 
     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
@@ -39,6 +63,10 @@ $(document).ready(function() {
       $("#show-contact h2").text(newContact.fullName());
       $(".first-name").text(newContact.firstName);
       $(".last-name").text(newContact.lastName);
+      $("ul#addresses").text("");
+      newContact.addresses.forEach(function(address) {
+        $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
+      });
     });
   });
 });
